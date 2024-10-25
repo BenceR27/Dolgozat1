49cf80a52b07714c1e6d965a473164cd09956d61
BenceR27 <113088440+BenceR27@users.noreply.github.com>
2024-10-25 11:43:17 +0200

    És ez is működik

--- Program.cs
+++ Program.cs
@@ -9,34 +9,41 @@ namespace Dolgozat1
 {
     internal class Program
     {
-        static void vegeredmeny() 
-        { 
-        Console.WriteLine("Add meg a vásárlás végösszegét");
-        int osszeg = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Van törzsvásárlói kártyája?");
-        var van = Console.ReadLine();
-        double vegosszeg5 = osszeg - osszeg * 0.05;
-        double vegosszeg10 = osszeg - osszeg * 0.1;
+        static void vegeredmeny()
+        {
+            Console.WriteLine("Add meg a vásárlás végösszegét");
+            int osszeg = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Van törzsvásárlói kártyája?");
+            var van = Console.ReadLine();
+            double vegosszeg5 = osszeg - osszeg * 0.05;
+            double vegosszeg10 = osszeg - osszeg * 0.1;
 
-            if (osszeg < 10000) 
-            { 
+            if (osszeg < 10000)
+            {
                 Console.WriteLine("A fizetendő összeg: " + osszeg);
             }
-            else if (osszeg >= 10000 && osszeg < 50000 && van == "nincs")
+            else if (osszeg >= 10000 && osszeg < 50000)
             {
-                Console.WriteLine("A fizetendő összeg: " + osszeg); 
-                if (osszeg >= 10000  && osszeg < 50000 && van == "van")
+                if (osszeg >= 10000 && osszeg < 50000 && van == "van")
                 {
                     Console.WriteLine("A fizetendő összeg: " + vegosszeg5);
                 }
+                else if (osszeg >= 10000 && osszeg < 50000 && van == "nincs")
+                {
+                    Console.WriteLine("A fizetendő összeg: " + osszeg);
+                }
             }
-            else if (osszeg >= 50000 && van == "nincs")
+            else if (osszeg >= 50000)
             {
-                Console.WriteLine("A fizetendő összeg: " + vegosszeg5);
                 if (osszeg >= 50000 && van == "van")
                 {
                     Console.WriteLine("A fizetendő összeg: " + vegosszeg10);
                 }
+                else if (osszeg >= 50000 && van == "nincs")
+                {
+                    Console.WriteLine("A fizetendő összeg: " + vegosszeg5);
+                }
+                
             }
 
         }
